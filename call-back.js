8cb867a8d68cb3b87e0c3a7f568ced46dcbd3e45
Rasha ali <rashaali84@gmail.com>
2020-04-29 05:07:10 +0200

    classes for explanation

---
+++ call-back.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+fs.readFile('input.txt', function (err, data) { //call back
+    if (err) {
+        console.log(err.stack);
+        return;
+    }
+    console.log(data.toString());
+});
+console.log("Program Ended");
+/* Here fs.readFile() 
+is a async function 
+whose purpose is to read a
+ file.If an error occurs 
+ during the read operation, 
+ then the err object will contain the corresponding error,
+  else data will contain the contents of the file.readFile 
+  passes err and data to the callback function after 
+  the read operation is complete, which finally prints the content.*/
